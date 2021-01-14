4872e66ffba47c315221f8242c63e333b59b1064
fernandoic <fernando_santos_ti@outlook.com>
2021-01-14 19:03:01 -0300

    Criando entity Post com User aninhado / Projeção dos dados do autor com DTO

---
+++ src/main/java/com/luizsilva/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.luizsilva.workshopmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luizsilva.workshopmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
