ec51a196a5eee5b013c31ec3904d492798d19084
fernandoic <fernando_santos_ti@outlook.com>
2021-02-05 00:12:09 -0300

    Consulta simples com query methods

--- src/main/java/com/luizsilva/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/luizsilva/workshopmongodb/resources/PostResource.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luizsilva.workshopmongodb.domain.Post;
 import com.luizsilva.workshopmongodb.domain.User;
+import com.luizsilva.workshopmongodb.resources.util.URL;
 import com.luizsilva.workshopmongodb.services.PostService;
 
 @RestController
@@ -29,4 +31,15 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value= "text", defaultValue="") String text) {
+
+		text=URL.decodeParam(text);
+		
+		List<Post> list =service.findByTitle(text);
+
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
--- src/main/java/com/luizsilva/workshopmongodb/services/PostService.java
+++ src/main/java/com/luizsilva/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.luizsilva.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,10 @@ public class PostService {
 		}
 
 		return user;
+	}
+	
+	public List<Post> findByTitle (String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
